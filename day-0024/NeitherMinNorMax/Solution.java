0fde647347ba00f2496baa404811d658b394b965
kunal <kunalgupta82623@gmail.com>
2023-09-09 23:52:44 +0530

    CrackYourPlacement

---
+++ day-0024/NeitherMinNorMax/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Solution {
+    public int findNonMinOrMax(int[] nums) {
+        if(nums.length<3)
+        {
+            return -1;
+        }
+       Arrays.sort(nums);
+       return nums[1];
+    }
+}
